db4e6e82830dcbae1feafeb2db917d831cc71eda
Uddam Darren <84756625+Iunasia@users.noreply.github.com>
2026-03-19 13:57:22 +0700

    some fill in routing

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,9 +1,8 @@
+import { useAuth } from '../context/AuthContext'
 import { Navigate, Outlet } from 'react-router-dom'
 
 export default function PrivateRoute() {
-  const currentUser = JSON.parse(localStorage.getItem('currentUser') || 'null')
-
-  if (!currentUser) return <Navigate to="/login" replace />
-
+  const { user } = useAuth()
+  if (!user) return <Navigate to="/login" replace />
   return <Outlet />
 }
